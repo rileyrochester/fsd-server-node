0fb92ae2f353de20a290cc6dab0e35eede2f58d1
Riley Rochester <rochester.r@northeastern.edu>
2022-04-08 18:22:10 -0400

    complete pt5 of hw doc

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import helloController  from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
 const app = express();
+app.use(express.json());
 helloController(app);
 userController(app);
 app.listen(4000);
