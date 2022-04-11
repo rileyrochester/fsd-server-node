82fd6d8bb83db0e8c5bfb241604d3215ce98f6ff
Riley Rochester <rochester.r@northeastern.edu>
2022-04-11 13:59:33 -0400

    tuitscontroller

---
+++ controllers/tuits-controller.js
@@ -0,0 +1,36 @@
+import posts from './tuits.js';
+let tuits = posts;
+
+const tuitsController = (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findAllTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
+
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime()+'';
+    newTuit.likes = 0;
+    tuits.push(newTuit);
+    res.json(newTuit);
+}
+
+const findAllTuits = (req, res) =>
+    res.json(tuits);
+
+const updateTuit = (req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updatedTuit = req.body;
+    tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
+    res.sendStatus(200);
+}
+
+const deleteTuit = (req, res) => {
+    const tuitIdToDelete = req.params.tid;
+    tuits = tuits.filter(t => t._id !== tuitIdToDelete);
+    res.sendStatus(200);
+}
+
+
+export default tuitsController;
