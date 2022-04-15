0608df2eb1a71bd1881de91ba3f1c2dccff55f00
Riley Rochester <rochester.r@northeastern.edu>
2022-04-15 15:43:52 -0400

    started a9 oops

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://localhost:27017/webdev');
 import helloController  from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
 import tuitsController  from "./controllers/tuits-controller.js";
