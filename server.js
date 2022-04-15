238b02a0b9bc0f42ff2e46e750b281954c395c2f
Riley Rochester <rochester.r@northeastern.edu>
2022-04-15 19:33:26 -0400

    shouldnt have had mongoose oops

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost:27017/webdev');
 import helloController  from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
 import tuitsController  from "./controllers/tuits-controller.js";
