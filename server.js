e1212a177eeb7e7e4806e987556dbbc858e7cb72
Riley Rochester <rochester.r@northeastern.edu>
2022-04-15 20:36:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost:27017/webdev');
 import helloController  from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
 import tuitsController  from "./controllers/tuits-controller.js";
+mongoose.connect('mongodb://localhost:27017/webdev');
 const app = express();
 app.use(express.json());
 app.use(cors());
