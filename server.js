4994f490de0f09ec662622829645bb729b59a2c7
Riley Rochester <rochester.r@northeastern.edu>
2022-04-15 21:34:26 -0400

    fixed :-:

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ import mongoose from 'mongoose';
 import helloController  from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
 import tuitsController  from "./controllers/tuits-controller.js";
-mongoose.connect('mongodb://localhost:27017/webdev');
+//mongoose.connect('mongodb://localhost:27017/webdev');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/webdev';
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
 app.use(cors());
 helloController(app);
 userController(app);
 tuitsController(app);
-app.listen(5000);
-//app.listen(process.env.PORT || 4000);
+//app.listen(4000);
+app.listen(process.env.PORT || 4000);
